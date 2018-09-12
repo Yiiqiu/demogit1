003b5c1055d0f133d6096c3d465642eb1ca50cbb
gongys1 <gongys1@ifeng.com>
2018-09-12 10:37:00 +0800

    System.out.println("第二个dev 修改 a");

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("dev 修改 a");
+
+        System.out.println("第二个dev 修改 a");
     }
 }
