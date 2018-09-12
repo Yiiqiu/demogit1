825bb6bc581a19fd66f66426ae30b40d71c04d63
gongys1 <gongys1@ifeng.com>
2018-09-12 11:05:13 +0800

    aaa
    # Conflicts:
    #	src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,9 @@ public class Main {
         System.out.println("master");
         System.out.println("master");
         System.out.println("master");
+        System.out.println("master2");
+        System.out.println("master2");
+        System.out.println("master2");
 
     }
 }
