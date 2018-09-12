3275413d71dcc2b8ef69e98c9dfba8d3c5389ec2
gongys1 <gongys1@ifeng.com>
2018-09-12 10:55:42 +0800

    private int d=100;

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     private int a=100;
     private int b=100;
     private int c=100;
+    private int d=100;
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("dev 修改 a");
