bc97922a592fa3cf962ae741e5f124d8938d39e6
gongys1 <gongys1@ifeng.com>
2018-09-12 10:59:53 +0800

    delete  System.out.println("第二个dev 修改 a");
            System.out.println("master修改 a");

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("dev 修改 a");
-        System.out.println("第二个dev 修改 a");
-
-        System.out.println("master修改 a");
 
 
 
