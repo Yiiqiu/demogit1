e2abf56202364df03e4ebab3d8ad51e274b72ae7
gongys1 <gongys1@ifeng.com>
2018-09-12 11:37:55 +0800

    System.out.println("444444444444");

--- src/Main.java
+++ src/Main.java
@@ -18,6 +18,7 @@ public class Main {
         System.out.println("EEEEEEEEEEEEEEE");
         System.out.println("111111111111");
         System.out.println("22222222222222");
+        System.out.println("444444444444");
 
 
 
